ca968eb07da1846b28708aa0cbaa873c013d7b90
Volodymyr Vakhotskyi <vaahoot@gmail.com>
2026-04-21 01:48:56 +0100

    updated link for v0.1.2

--- Formula/zen-mode.rb
+++ Formula/zen-mode.rb
@@ -1,7 +1,7 @@
 class ZenMode < Formula
   desc "A macOS CLI tool that blocks distracting domains"
   homepage "https://github.com/vaahoot/zen-mode"
-  url "https://github.com/vaahoot/zen-mode/archive/refs/tags/v0.1.2.tar.gz"
+  url "https://github.com/vaahoot/zen-mode/releases/download/v0.1.2/zen-0.1.2.tar.gz"
   sha256 "ccbfd82f3e5923aafd07baa8001256d7cda8caf63c36c20a04c7cef98f56c2bc"
   license "MIT"
 
